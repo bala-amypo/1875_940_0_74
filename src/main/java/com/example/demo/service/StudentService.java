77722d69c8b589e52763455ed7eab5f78fbf2d83
bala-amypo <bala@amypo.in>
2025-12-17 09:56:24 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,4 +1,10 @@
-package com.example.demo.service;
-public interface StudentService{
-    
+package com.example.aiml.service;
+
+
+import com.example.aiml.entity.Student;
+
+
+public interface StudentService {
+    Student saveStudent(Student student);
+
 }
